3ed00a7fed9d10e8c6fd4594d1bd6027516fea9f
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2016-01-05 17:38:31 +0900

    add DisabledGroup

---
+++ AffinityUI/Editor/DisabledGroup.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace AffinityUI {
+	public class DisabledGroup : Composite<DisabledGroup> {
+		BindableProperty<DisabledGroup, bool> isDisabled;
+
+		public DisabledGroup() {
+			isDisabled = new BindableProperty<DisabledGroup, bool>(this);
+		}
+
+		public DisabledGroup OnDisabled(PropertyChangedEventHandler<DisabledGroup, bool> handler) {
+			isDisabled.PropertyChanged += handler;
+			return this;
+		}
+
+		public BindableProperty<DisabledGroup, bool> IsDisabled() {
+			return isDisabled;
+		}
+
+		public DisabledGroup IsDisabled(bool value) {
+			isDisabled.Value = value;
+			return this;
+		}
+
+		protected override void OnBeginLayout_GUI () {
+			EditorGUI.BeginDisabledGroup(isDisabled);
+		}
+
+		protected override void OnEndLayout_GUI () {
+			EditorGUI.EndDisabledGroup();
+		}
+
+		protected override void OnBeginLayout_GUILayout () {
+			EditorGUI.BeginDisabledGroup(isDisabled);
+		}
+
+		protected override void OnEndLayout_GUILayout () {
+			EditorGUI.EndDisabledGroup();
+		}
+	}
+}
